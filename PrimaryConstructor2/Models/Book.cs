98fe9943e939be94ca33969df9315c7f2d342f75
Christian Nagel <christian@christiannagel.com>
2023-11-01 15:04:09 +0100

    primary constructor issue with ef core context

---
+++ PrimaryConstructor2/Models/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace Scaffolding1.Models;
+
+public class Book(int id, string title, string? publisher = default)
+{
+    public int Id { get; set; } = id;
+    public string Title { get; set; } = title;
+    public string? Publisher { get; set; } = publisher;
+}
---
+++ PrimaryConstructor2/Models/BooksContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Scaffolding1.Models;
+
+public class BooksContext(DbContextOptions<BooksContext> options) : DbContext(options)
+{
+    public DbSet<Book> Book => Set<Book>();
+}
